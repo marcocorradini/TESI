539a31cdce4f4928a6004fc9f93d424a1b12e5b3
marcocorradini <114239851+marcocorradini@users.noreply.github.com>
2022-10-19 08:57:40 +0200

    Delete get_dynamics.m

--- get_dynamics.m
+++
@@ -1,9 +0,0 @@
-function [B,C,g] = get_dynamics(q,dq,param)
-
-    % Raggruppo le funzioni  get_B,  get_C e  get_g per avere solo un'unica funzione nel main
-    
-    B = get_B(q,param);
-    C = get_C(q,dq,param);
-    g = get_g(q,param);
-end
-
