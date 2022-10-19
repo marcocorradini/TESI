6360b774fb1bfc2bf20d5f5ae92ce35cdee82c4f
Matteo Saveriano <matteo.saveriano@uibk.ac.at>
2022-10-19 14:55:48 +0200

    Passive computed torque works

--- Passivity_test_CT2.m
+++ Passivity_test_CT2.m
@@ -1,9 +1,9 @@
-function [dW1] = Passivity_test_CT2(q,dq,B,C,Kd,Kp,e,tau,z,dz,T)
+function [dW1] = Passivity_test_CT2(q,dq,B,C,Kd,Kp,e,tau,z,dz,T,alpha)
 %Funzione che permette di calcolare l'energia e la potenza per il calcolo
 %della passività del robot
             
         W1 = 0.5*dq'*B*dq+T;
     
-        dW1 = dq'*C*dq - dq'*B*Kd*dq + dq'*B*Kp*e + dq'*tau' + z*dz;
+        dW1 = dq'*C*dq - dq'*B*Kd*dq + alpha*dq'*B*Kp*e + dq'*tau' + z*dz;
 end
 
